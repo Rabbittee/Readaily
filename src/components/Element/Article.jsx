5571e823b36b6b9905dd58492d2c9700c8a2a74a
Erica <erica@EricadeMac-mini.local>
2022-04-25 14:56:32 +0800

    fix: remove env file

--- src/components/Element/Article.jsx
+++ src/components/Element/Article.jsx
@@ -10,34 +10,6 @@ export function Article() {
   const [link, setLink] = useState('');
   const markRef = useRef(null);
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_URL}/api/article`)
-      .then((res) => {
-        setArticle(
-          res.data.articles.reduce((acc, curr) =>
-            curr.content.length > acc.content.length ? curr : acc
-          )
-        );
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
-  useEffect(() => {
-    if (!sentence) return;
-    getTipPosition();
-    setLink(translateURL());
-    window.addEventListener('scroll', getTipPosition);
-    window.addEventListener('pointerdown', hideTip);
-
-    return () => {
-      window.removeEventListener('scroll', getTipPosition);
-      window.removeEventListener('pointerdown', hideTip);
-    };
-  }, [sentence]);
-
   function getTipPosition() {
     const { scrollY } = window;
     setPosition({
@@ -56,12 +28,13 @@ export function Article() {
     setSentence(() => {
       const text = article.content;
       const word = text.substring(start, end);
-      if (!word.trim()) return false;
-      return {
-        prev: text.substring(0, start),
-        word: word,
-        next: text.substring(end, text.length),
-      };
+      if (word.trim()) {
+        return {
+          prev: text.substring(0, start),
+          word: word,
+          next: text.substring(end, text.length),
+        };
+      }
     });
   }
 
@@ -70,19 +43,49 @@ export function Article() {
   }
 
   function translateURL() {
-    if (!sentence.word) return;
-    const url = 'https://translate.google.com.tw';
-    const search = {
-      hl: 'zh-TW',
-      tab: 'rT',
-      sl: 'en',
-      tl: 'zh-TW',
-      text: sentence.word.trim(),
-      op: 'translate',
-    };
-    const result = `${url}/?` + new URLSearchParams({ ...search });
-    return result.toString().split('+').join('%20');
+    if (sentence.word) {
+      const url = 'https://translate.google.com.tw';
+      const search = {
+        hl: 'zh-TW',
+        tab: 'rT',
+        sl: 'en',
+        tl: 'zh-TW',
+        text: sentence.word.trim(),
+        op: 'translate',
+      };
+      const result = `${url}/?` + new URLSearchParams({ ...search });
+      return result.toString().split('+').join('%20');
+    }
   }
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_URL}/api/article`)
+      .then((res) => {
+        setArticle(
+          res.data.articles.reduce((acc, curr) =>
+            curr.content.length > acc.content.length ? curr : acc
+          )
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (sentence) {
+      getTipPosition();
+      setLink(translateURL());
+      window.addEventListener('scroll', getTipPosition);
+      window.addEventListener('pointerdown', hideTip);
+    }
+
+    return () => {
+      window.removeEventListener('scroll', getTipPosition);
+      window.removeEventListener('pointerdown', hideTip);
+    };
+  }, [sentence]);
 
   return (
     <article className={clsx('px-10')}>
